8748e1ab152046b1544416e22bcf869be2d57029
DRed <dima.redkin.1999@gmail.com>
2020-06-22 23:06:37 +0500

    DbSet никогда не будет принимать значение null, но для компилятора это неизвестно, поэтому я отключил warning для DbSet принудительно с помощью (!). https://docs.microsoft.com/ru-ru/ef/core/miscellaneous/nullable-reference-types#dbcontext-and-dbset

--- DAL/NotesDbContext.cs
+++ DAL/NotesDbContext.cs
@@ -5,7 +5,7 @@ namespace Notes.DAL
 {
     public class NotesDbContext : DbContext
     {
-        public DbSet<Note> Notes { get; set; }
+        public DbSet<Note> Notes { get; set; } = null!;
 
         public NotesDbContext(DbContextOptions<NotesDbContext> options)
             : base(options)
