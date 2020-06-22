c0afe61c422718bf02164828aff3bcd6986c4063
DRed <dima.redkin.1999@gmail.com>
2020-06-22 22:46:33 +0500

    заново установил Microsoft.EntityFrameworkCore.Design, оказалось он нужен для создания миграций. Включил для проекта фичу C# 8.0 - nullable reference types. Сделал заголовок заметки nullable. Также добавил конструктор для заметки

--- DAL/Entities/Note.cs
+++ DAL/Entities/Note.cs
@@ -1,12 +1,23 @@
-﻿﻿using System;
+﻿using System;
 
 namespace Notes.DAL.Entities
 {
     public class Note
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public string Content { get; set; }
         public DateTime DateOfLastChange { get; set; }
+
+        public Note(string content)
+        {
+            Content = content;
+        }
+
+        public Note(string title, string content)
+        {
+            Title = title;
+            Content = content;
+        }
     }
 }
---
+++ DAL/Migrations/20200622174327_AddNotNullConstraintToTitlePropertyOfNote.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Notes.DAL.Migrations
+{
+    public partial class AddNotNullConstraintToTitlePropertyOfNote : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Notes",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Notes",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
